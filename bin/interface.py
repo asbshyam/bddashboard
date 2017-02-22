25916e36bcf05e51b50c2524728a820f909d5b55
asbshyam <asbshyam@gmail.com>
2017-02-22 10:00:32 +0800

    Update interface.py

--- bin/interface.py
+++ bin/interface.py
@@ -7,15 +7,21 @@ import begin
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
+@app.route('/test')
+def test():
     begin.logic()
     string = ""
-    infile = open("temp.txt", "r").read()
+    infile = open("data/temp.txt", "r").read()
     for line in infile.splitlines():
         string += str(line.split(",")[1]) + str("<br>")
         print string
     return string
 
+
+@app.route('/')
+def root():
+    string = "<html><title>Test</title><body><a href=\"test\">Test Page</body></html>"
+    return string
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
